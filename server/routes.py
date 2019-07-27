91176a947a1265656ff34892f6df0c6b67ffabe9
AD9000 <damle.atharv16@gmail.com>
2019-07-28 02:49:43 +1000

    fixed backend persistence issues across backend api requests

--- server/routes.py
+++ server/routes.py
@@ -1,14 +1,24 @@
 from flask import Flask
 from flask import request
+from flask import session
 from flask_restful import Api, Resource, reqparse
 import apiUtility
 import datetime
+import json
+import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 api = Api(app)
 
 class User(Resource):
+    # isflagged tells whether a bus is flagged at a stop or not.
+    # if there is a key with the bus-id, then it is flagged at specified stops...
+    # bus (key) -> [{ stop (key) -> number of people flagging (value) }] (value)
+    # def __init__(self):
+    #     super()
+    #     isFlagged = {}
+
     def get(self):
         if request.args != {}:
             try:
@@ -24,13 +34,82 @@ class User(Resource):
             except:
                 return "no bus stop found", 400
         else:
-            return "no arguments!", 400
-        # if (request.args is {} or request.args['lat'] is None or request.args['long'] is None):
-        #     return 'invalid input!'
-        
+            return "no arguments!", 400        
 
     def post(self):
-        return request.args
+        #path = './tempfile'
+        #flaginfo = request.args['']
+        try:
+            # flag a certain stop
+            busId = (int(request.args['busNumber']), int(request.args['tripCode'])) 
+            busId = str(busId)
+            #return busId
+            stopId = int(request.args['stopId'])
+            stopId = str(stopId)
+            flag = str(bool(request.args['flag'])).lower() == 'true'
+
+            isFlagged = {}
+            if os.path.exists('tempfile') and os.path.getsize('tempfile') > 0:
+                # load data from file
+                isFlagged = self.load('tempfile')
+
+            keys = isFlagged.keys()
+            #return isFlagged
+            #return busId in keys
+            # if the bus is flagged, at some stop...
+            if (busId in keys): 
+                # return 'worked kind of'
+                # if the bus is flagged at a certain
+                
+                # for each stop the bus is flagged at...
+                for stop in isFlagged[busId]:
+                    innerkeys = stop.keys()
+                    # print ("so stop id in keys!!\n\n\n\n\n\n\n")
+                    #return innerkeys
+                    if (stopId in innerkeys):
+                        if (flag):
+                            stop[stopId] += 1
+                        else:
+                            if (stop[stopId] == 0):
+                                print ('unexpected unflag when flag = 0')
+                            else:
+                                stop[stopId] -= 1
+                    else:
+                        if (flag):
+                            stop[stopId] = 1
+                        else:
+                            print ('unexpected unflag when flag does not exist', 400)
+            else:
+                if (flag):
+                    isFlagged[busId] = [{ stopId: 1 }]
+                else:
+                    return 'unexpected unflag when bus was never flagged', 400
+
+            self.save(isFlagged, 'tempfile')
+            return 'got flagged!\n' + str(isFlagged), 200
+        except Exception as e:
+            return 'error when flagging!' + str(e), 400
+
+    # saves the flagged dict.
+    def save(self, flagged, file):
+        try:
+            print ('saving...' + str(file))
+            f = open(file, 'w')
+            f.write(json.dumps(flagged))
+            f.close()
+        except Exception as e:
+            print ("error while opening file: " + str(e))
+
+    # loads the saved dict into the current one.
+    def load(self, file):
+        try:
+            f = open(file, 'r')
+            flagged = json.loads(f.read())
+            f.close()
+
+            return flagged
+        except:
+            print ("error while opening file for loading")
     
 api.add_resource(User, "/")
 app.run(debug=True)
