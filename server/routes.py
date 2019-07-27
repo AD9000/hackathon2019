613c7ef8f4e91a77bff91d75bf887fa381b4c6a3
AD9000 <damle.atharv16@gmail.com>
2019-07-27 18:40:38 +1000

    routes working

--- server/routes.py
+++ server/routes.py
@@ -1,79 +1,25 @@
 from flask import Flask
+from flask import request
 from flask_restful import Api, Resource, reqparse
 import apiUtility
+import datetime
 
 app = Flask(__name__)
 api = Api(app)
 
-users = [
-    {
-        "name": "a",
-        'age': 10,
-        'occ': "occ1"
-    },
-    {
-        "name": "b",
-        'age': 20,
-        'occ': "occ2"
-    }
-]
-
-
 class User(Resource):
-    def get(self, name):
-        # Get the data from the api
-        #coords = 
-        # Fake data for now!
-        for user in users:
-            if (name == user['name']):
-                return user, 200
-        
-        return 'user not found'
+    def get(self):
+        # get the closest bus stop...
+        stop = apiUtility.get_stop_by_location(request.args['lat'], request.args['long'])
 
-    def post(self, name):
-        parser = reqparse.RequestParser()
-        parser.add_argument("age")
-        parser.add_argument("occupation")
-        args = parser.parse_args()
+        # get the list of buses...
+        buses = apiUtility.get_depart_from_stop(stop["id"])
 
-        for user in users:
-            if (name == user['name']):
-                return 'user with name exists'
-        
-        user = {
-            'name': name,
-            'age': args['age'],
-            'occupation': args['occupation']
-        }
+        # returns all the buses from the closest stop. utc time tho...
+        return buses
 
-        users.append(user)
-        return user, 201
-
-    def put(self, name):
-        parser = reqparse.RequestParser()
-        parser.add_argument("age")
-        parser.add_argument("occupation")
-        args = parser.parse_args()
-
-        for user in users:
-            if (name == user['name']):
-                user['age'] = args['age']
-                user['occupation'] = args['occupation']
-                return user, 200
-        
-        user = {
-            'name': name,
-            'age': args['age'],
-            'occupation': args['occupation']
-        }
-
-        users.append(user)
-        return user, 201
+    def post(self):
+        return request.args
     
-    def delete(self, name):
-        global users
-        users = [user for user in users if user['name'] != name]
-        return "{} is deleted.".format(name), 200
-    
-api.add_resource(User, "/user/<string:name>")
+api.add_resource(User, "/")
 app.run(debug=True)
